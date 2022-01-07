2d26d887e44a3e296004f7c768edf16195242528
qhtjd2131 <qhtjd2131@gmail.com>
2022-01-07 22:49:51 +0900

    first commit, CRA없이 babel, wepack, react 를 조합하여 서버구동, 화면랜더링 완료

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>React App Without CRA!-----!</div>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env","@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
